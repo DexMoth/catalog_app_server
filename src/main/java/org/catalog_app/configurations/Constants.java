066c08b6fe9d6927bb9f01863c075b7e75ffe0b6
dex_moth <dovakin042@mail.ru>
2025-12-02 09:38:45 +0400

    сервер готов

---
+++ src/main/java/org/catalog_app/configurations/Constants.java
@@ -0,0 +1,9 @@
+package org.catalog_app.configurations;
+
+public class Constants {
+    public static final String SEQUENCE_NAME = "hibernate_sequence";
+    public static final String API_URL = "/api";
+
+    private Constants() {
+    }
+}
